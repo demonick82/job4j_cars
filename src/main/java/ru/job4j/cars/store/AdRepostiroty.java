9c2a36021600f22b54c0bf54963945db82237d10
demonick82 <demonick82@gmail.com>
2021-12-21 22:45:19 +0300

    Реализовать пользовательские фильтры для площадок машин [#4745] (Исправления)

--- src/main/java/ru/job4j/cars/store/AdRepostiroty.java
+++ src/main/java/ru/job4j/cars/store/AdRepostiroty.java
@@ -8,9 +8,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.cars.model.Model;
 import ru.job4j.cars.model.Post;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -23,12 +21,12 @@ public class AdRepostiroty {
 
 
     public Collection<Post> findPostToday() {
-        LocalDateTime start = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
-        LocalDateTime end = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
+        LocalDateTime end = LocalDateTime.now();
+        LocalDateTime start = end.minusDays(1);
+
         Session session = sf.openSession();
         session.beginTransaction();
-
-        List result = session.createQuery(
+        List<Post> result = session.createQuery(
                 "select distinct p from Post p  "
                         + "join fetch p.photos ph "
                         + " where p.created between :strDate and :endDate")
